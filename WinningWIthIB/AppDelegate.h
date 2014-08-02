e0201e593cd6f65e5f23e4e490be6f0823b093e5
Jack Cox <jcox@captechventures.com>
2014-08-02 15:01:04 -0400

    Initial Commit

---
+++ WinningWIthIB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WinningWIthIB
+//
+//  Created by Jack Cox on 8/2/14.
+//  Copyright (c) 2014 CapTech Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WinningWIthIB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WinningWIthIB
+//
+//  Created by Jack Cox on 8/2/14.
+//  Copyright (c) 2014 CapTech Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
